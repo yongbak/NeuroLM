e45e6a082211c03969dc2dd751e77f50d4a1dd26
yongbak <yongbakk@gmail.com>
2025-11-13 21:52:44 +0900

    Update train_vq.py
    bug fix

--- train_vq.py
+++ train_vq.py
@@ -185,7 +185,7 @@ def main(args):
     checkpoint = None # free up memory
 
     # compile the model
-    if compile:
+    if args.compile:
         print("compiling the model... (takes a ~minute)")
         unoptimized_model = model
         model = torch.compile(model) # requires PyTorch 2.0
