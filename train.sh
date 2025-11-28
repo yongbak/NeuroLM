406b02b06ce108561baa9634a8ce34a35c902082
YongHyeonPARK <30647875+SYHPARK@users.noreply.github.com>
2025-11-28 11:01:38 +0900

    update train script

--- train.sh
+++ train.sh
@@ -1,1 +1,9 @@
-python train_vq.py --dataset_dir datasets/processed/PMD_samples --text_dataset_dir datasets/ --out_dir ./vq_output --epochs 50 --warmup_epochs 10 --learning_rate 1e-4
+python train_vq.py \
+--dataset_dir datasets/processed/PMD_samples \
+--text_dataset_dir datasets/ \
+--out_dir ./vq_output \
+--epochs 50 \
+--warmup_epochs 10 \
+--learning_rate 3e-5 \
+--grad_clip 0.5 \
+--dead_code_threshold 1.0
